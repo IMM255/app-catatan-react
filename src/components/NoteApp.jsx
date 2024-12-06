faf6a54a5d031cc145591b47fe168bb39c440abb
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-06 15:40:43 +0700

    add delete button

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import { getInitialData } from "../utils";
 import NoteList from "./NoteList";
 
-function NoteApp(){
-    const notes = getInitialData();
+class NoteApp extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            notes: getInitialData(),
+        }
+
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    }
+    onDeleteHandler(id){
+        const notes = this.state.notes.filter(note => note.id !== id);
+        this.setState({ notes })
+    }
+    render(){
     return (
         <div className="note-app">
-            <NoteList notes={notes} />
+            <h2>Catatan Aktif</h2>
+            <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler} />
         </div>
     )
+    }
 }
 
 export default NoteApp;
--- src/components/NoteItemAction.jsx
+++ src/components/NoteItemAction.jsx
@@ -5,6 +5,9 @@ function NoteItemAction ({id, onDelete }) {
     return (
         <div className="note-item__action">
             <DeleteButton id={id} onDelete={onDelete} />
+
         </div>
     )
 } 
+
+export default NoteItemAction;
